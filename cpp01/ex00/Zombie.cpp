95b876a567ad3c799eb0f126014a1615a167f051
Mikayel Arzumanyan <mikarzum@c2r11s1.42yerevan.am>
2023-08-26 22:21:37 +0400

    asd

---
+++ cpp01/ex00/Zombie.cpp
@@ -0,0 +1,7 @@
+#include "Zombie.hpp"
+
+void announce(void)
+{
+	std::cout << "";
+	std::cout << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
---
+++ cpp01/ex00/randomChump.cpp
@@ -0,0 +1,5 @@
+	
+void randomChump(std::string name)
+{
+
+}
